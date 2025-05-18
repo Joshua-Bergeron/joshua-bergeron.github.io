47077a99e563c110dff75c29ff05aaf59f944fcc
Joshua Bergeron <pacluc20@aol.com>
2025-05-17 17:21:21 -0700

    move content to new directory

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,44 +1,9 @@
 import React from "react";
 import { Box, Typography, Paper, Grid } from "@mui/material";
 import background from "../assets/background2.jpg";
+import events from "../content/ExperienceContent";
 
 function Experience() {
-  const events = [
-    {
-      year: "June 2023 - Present",
-      title: "Associate Software Engineer in Test",
-      company: "Company Name",
-      logo: "https://logo.clearbit.com/https://www.ashcompanies.com/",
-      description: [
-        "Formulate comprehensive UI tests in Playwright to assess the functionality of components and sites.",
-        "Write and manage automated Postman API tests to ensure the robustness of company software.",
-        "Construct and develop automated CI/CD test pipelines with Azure DevOps.",
-        "Expose and identify bugs in development, testing, staging, and production environments.",
-        "Write JMeter test plans to load-test company applications and sites under heavy customer use.",
-      ],
-    },
-    {
-      year: "June 2022 - August 2022",
-      title: "IT Intern",
-      company: "Kros-Wise Consulting",
-      logo: "https://logo.clearbit.com/kros-wise.com",
-      description: [
-        "Assisted employees with IT-related issues such as troubleshooting and upgrading user hardware.",
-        "Wrote comprehensive documentation for IT policies and procedures such as user account creation and software installations.",
-      ],
-    },
-    {
-      year: "November 2019 - June 2023",
-      title: "Lead Math Instructor",
-      company: "Mathnasium",
-      logo: "https://logo.clearbit.com/mathnasium.com",
-      description: [
-        "Instructed K-12 students in various levels of math (through calculus).",
-        "Trained new tutors, managed students’ curriculums, and maintained an organized learning environment.",
-      ],
-    },
-  ];
-
   return (
     <Box
       id="experience"
